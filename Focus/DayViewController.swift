35b616462d578c15580f8e3c5e5e2d0e2b502bf2
Jeremy Friedland <jeremyfriedland@gmail.com>
2016-06-19 16:22:08 -0700

    Hooked up task view controller to day view and got panning gesture on tray to work.

--- Focus/DayViewController.swift
+++ Focus/DayViewController.swift
@@ -13,15 +13,40 @@ class DayViewController: UIViewController, UITableViewDataSource, UITableViewDel
     @IBOutlet weak var navItem: UINavigationItem!
     @IBOutlet weak var eventsTableView: UITableView!
     
+
+    @IBOutlet weak var taskTrayView: UIView!
+    
+    //Var for tray
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var taskViewController: UIViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //Add Task view controller to tasy tray view
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        taskViewController = storyboard.instantiateViewControllerWithIdentifier("TasksViewController")
+        taskTrayView.addSubview(taskViewController.view)
 
-        // Do any additional setup after loading the view.
+        //Set Tray View Values
+        trayDownOffset = 200
+        trayUp = taskTrayView.center
+        trayDown = CGPoint(x: taskTrayView.center.x, y: taskTrayView.center.y + trayDownOffset)
+        
+        //Set Tray closed on Load
+        taskTrayView.center = trayDown
+
+        
         
         eventsTableView.delegate = self
         eventsTableView.dataSource = self
         
         navItem.title = "Today"
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,6 +89,41 @@ class DayViewController: UIViewController, UITableViewDataSource, UITableViewDel
         return cell
         
     }
+    
+    @IBAction func didPanTray(sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            trayOriginalCenter = taskTrayView.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            
+            taskTrayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            if velocity.y > 0 {
+                // move down
+                
+                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                    () -> Void in self.taskTrayView.center = self.trayDown
+                    }, completion: { (Bool) -> Void in
+                })
+                
+            } else {
+                // move up
+                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                    () -> Void in self.taskTrayView.center = self.trayUp
+                    }, completion: { (Bool) -> Void in
+                })
+            }
+        }
+    }
+
     
 
 }
